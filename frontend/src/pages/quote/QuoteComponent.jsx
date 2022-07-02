d56e74afc9577d79e46063f5e57c67a96cfeda38
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-07-03 00:03:01 +0530

    quote component

---
+++ frontend/src/pages/quote/QuoteComponent.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Resizable } from "re-resizable";
+import styles from "./QuoteComponent.module.css";
+
+const QuoteComponent = () => {
+  return (
+    <div className={styles.quote}>
+      <Resizable
+        defaultSize={{
+          width: "90vw",
+          height: "60vh",
+        }}
+      >
+        <h1>Quote of the day is</h1>
+        <p>
+            See, you not only have to be a good coder to create a system like Linux, you have to be a sneaky bastard too ;-)
+        </p>
+      </Resizable>
+    </div>
+  );
+};
+export default QuoteComponent;
