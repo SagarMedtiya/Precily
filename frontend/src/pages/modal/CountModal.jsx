81f7b9aa003c12181f13f8c44c195c4e630050e8
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-07-03 00:02:10 +0530

    countmodal component

---
+++ frontend/src/pages/modal/CountModal.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import { Modal, Header, Button} from "semantic-ui-react";
+
+import axios from "axios";
+
+
+const CountModal = ({ news }) => {
+  const [open, setOpen] = useState(false);
+
+  const [apiCalls, setApiCalls] = useState(-1);
+
+  useEffect(() => {
+    axios
+      .get("/count")
+      .then((response) => {
+        setApiCalls(response.data.count);
+      })
+      .catch((err) => console.log(err));
+  }, [news]);
+
+  return (
+    <Modal
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}
+      open={open}
+      trigger={
+        <Button onClick=size="large" color="violet">
+          API count
+        </Button>
+      }
+    >
+      <Modal.Header>API Calls </Modal.Header>
+      <Modal.Content image>
+        
+        <Modal.Description>
+          <Header>Total Number of API Calls</Header>
+
+          {apiCalls < 0 ? (
+            <p>No API calls have been made</p>
+          ) : (
+            <p>
+              <strong>API calls : {apiCalls}</strong>
+            </p>
+          )}
+
+          <Button color="black" onClick={() => setOpen(false)}>
+            Close
+          </Button>
+        </Modal.Description>
+      </Modal.Content>
+    </Modal>
+  );
+};
+
+export default CountModal;
